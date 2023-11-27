73e657d699b07ab54a4708cfc269bd068dc5f068
Chire <bbeny123@users.noreply.github.com>
2023-11-27 20:49:23 +0100

    typo fix

--- mount-debian.sh
+++ mount-debian.sh
@@ -7,7 +7,7 @@ DEBIAN_EXT_DIR1_TARGET=$DEBIAN_CHROOT_DIR/mnt/Beny/
 DEBIAN_EXT_DIR2=/tmp/mnt/Beny-Kingston/
 DEBIAN_EXT_DIR2_TARGET=$DEBIAN_CHROOT_DIR/mnt/Beny-Kingston/
 
-if [ $(mount | grep $CHROOT_DIR | wc -l) -gt 0 ]; then
+if [ $(mount | grep $DEBIAN_CHROOT_DIR | wc -l) -gt 0 ]; then
   mkdir -p $DEBIAN_EXT_DIR1_TARGET
   if ! mountpoint -q $DEBIAN_EXT_DIR1_TARGET ; then
     mount -Br $DEBIAN_EXT_DIR1 $DEBIAN_EXT_DIR1_TARGET
