2ea0556149a35c16a7d31074530467ce1a23cd86
Chire <bbeny123@users.noreply.github.com>
2023-11-28 00:02:11 +0100

    mount-debian.sh

--- mount-debian.sh
+++ mount-debian.sh
@@ -2,19 +2,15 @@
 
 DEBIAN_CHROOT_DIR=$(readlink -f /opt/debian)
 
-DEBIAN_EXT_DIR1=/tmp/mnt/Beny/
-DEBIAN_EXT_DIR1_TARGET=$DEBIAN_CHROOT_DIR/mnt/Beny/
-DEBIAN_EXT_DIR2=/tmp/mnt/Beny-Kingston/
-DEBIAN_EXT_DIR2_TARGET=$DEBIAN_CHROOT_DIR/mnt/Beny-Kingston/
-
 if [ $(mount | grep $DEBIAN_CHROOT_DIR | wc -l) -gt 0 ]; then
-  mkdir -p $DEBIAN_EXT_DIR1_TARGET
-  if ! mountpoint -q $DEBIAN_EXT_DIR1_TARGET ; then
-    mount -o bind $DEBIAN_EXT_DIR1 $DEBIAN_EXT_DIR1_TARGET
-  fi
-
-  mkdir -p $DEBIAN_EXT_DIR2_TARGET
-  if ! mountpoint -q $DEBIAN_EXT_DIR2_TARGET ; then
-    mount -o bind $DEBIAN_EXT_DIR2 $DEBIAN_EXT_DIR2_TARGET
-  fi
+  for dir in /mnt/*; do
+    dir=$(readlink -f $dir)/
+    if ! echo "$CHROOT_DIR" | grep -q ^$dir; then
+      target_dir=$CHROOT_DIR/mnt/$(echo "$dir" | sed 's!^.*mnt/!!')
+      mkdir -p $target_dir
+      if ! mountpoint -q $target_dir; then
+        mount -o bind $dir $target_dir
+      fi
+    fi
+  done
 fi
