fae0666f51698d872578fdac7e63f447546f2390
Chire <bbeny123@users.noreply.github.com>
2023-11-27 20:22:10 +0100

    tab to double space

--- mount-debian.sh
+++ mount-debian.sh
@@ -10,13 +10,13 @@ DEBIAN_EXT_DIR2_TARGET=$DEBIAN_CHROOT_DIR/mnt/Beny-Kingston/
 DEBIAN_MOUNTED_DIR_COUNT="$(mount | grep $DEBIAN_CHROOT_DIR | wc -l)"
 
 if [ $DEBIAN_MOUNTED_DIR_COUNT -gt 0 ]; then
-	mkdir -p $DEBIAN_EXT_DIR1_TARGET
-	if ! mountpoint -q $DEBIAN_EXT_DIR1_TARGET ; then
-		mount -Br $DEBIAN_EXT_DIR1 $DEBIAN_EXT_DIR1_TARGET
-	fi
+  mkdir -p $DEBIAN_EXT_DIR1_TARGET
+  if ! mountpoint -q $DEBIAN_EXT_DIR1_TARGET ; then
+    mount -Br $DEBIAN_EXT_DIR1 $DEBIAN_EXT_DIR1_TARGET
+  fi
 
-	mkdir -p $DEBIAN_EXT_DIR2_TARGET
-	if ! mountpoint -q $DEBIAN_EXT_DIR2_TARGET ; then
-		mount -Br $DEBIAN_EXT_DIR2 $DEBIAN_EXT_DIR2_TARGET
-	fi
+  mkdir -p $DEBIAN_EXT_DIR2_TARGET
+  if ! mountpoint -q $DEBIAN_EXT_DIR2_TARGET ; then
+    mount -Br $DEBIAN_EXT_DIR2 $DEBIAN_EXT_DIR2_TARGET
+  fi
 fi
