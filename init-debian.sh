f2f175958e0a003734b8bd53c499cb27d9918508
Chire <bbeny123@users.noreply.github.com>
2023-11-27 18:10:22 +0100

    Cleanup

--- init-debian.sh
+++ init-debian.sh
@@ -2,145 +2,123 @@
 
 PATH=/opt/bin:/opt/sbin:/sbin:/bin:/usr/sbin:/usr/bin
 
-# Folder with Debian
-
-CHROOT_DIR=/tmp/mnt/Beny-Debian/entware/debian
+CHROOT_DIR=/opt/debian
+CHROOT_SERVICES_LIST=/opt/etc/chroot-services.list
 
 EXT_DIR1=/tmp/mnt/Beny/
 EXT_DIR1_TARGET=/mnt/Beny/
 EXT_DIR2=/tmp/mnt/Beny-Kingston/
 EXT_DIR2_TARGET=/mnt/Beny-Kingston/
 
-CHROOT_SERVICES_LIST=/opt/etc/chroot-services.list
-
 if [ ! -e "$CHROOT_SERVICES_LIST" ]; then
-    echo "Please, define Debian services to start in $CHROOT_SERVICES_LIST first!"
-    echo "One service per line. Hint: this is a script names from Debian's /etc/init.d/"
-    exit 1
+	echo "Please, define Debian services to start in $CHROOT_SERVICES_LIST first!"
+	echo "One service per line. Hint: this is a script names from Debian's /etc/init.d/"
+	exit 1
 fi
 
 MountedDirCount="$(mount | grep $CHROOT_DIR | wc -l)"
 
 mount_ext() {
-    if [ ! -z "$EXT_DIR1" ] && [ ! -z "$EXT_DIR1_TARGET" ]; then
-        mkdir -p $CHROOT_DIR/$EXT_DIR1_TARGET
-        if ! mountpoint -q $CHROOT_DIR/$EXT_DIR1_TARGET ; then 
-            mount -Br $EXT_DIR1 $CHROOT_DIR/$EXT_DIR1_TARGET		 
-        fi
-    fi
+	if [ ! -z "$EXT_DIR1" ] && [ ! -z "$EXT_DIR1_TARGET" ]; then
+		mkdir -p $CHROOT_DIR/$EXT_DIR1_TARGET
+		if ! mountpoint -q $CHROOT_DIR/$EXT_DIR1_TARGET; then
+			mount -Br $EXT_DIR1 $CHROOT_DIR/$EXT_DIR1_TARGET
+		fi
+	fi
 
-    if [ ! -z "$EXT_DIR2" ] && [ ! -z "$EXT_DIR2_TARGET" ]; then
-        mkdir -p $CHROOT_DIR/$EXT_DIR2_TARGET		
-        if ! mountpoint -q $CHROOT_DIR/$EXT_DIR2_TARGET ; then 
-            mount -Br $EXT_DIR2 $CHROOT_DIR/$EXT_DIR2_TARGET		 
-        fi
-    fi
+	if [ ! -z "$EXT_DIR2" ] && [ ! -z "$EXT_DIR2_TARGET" ]; then
+		mkdir -p $CHROOT_DIR/$EXT_DIR2_TARGET
+		if ! mountpoint -q $CHROOT_DIR/$EXT_DIR2_TARGET; then
+			mount -Br $EXT_DIR2 $CHROOT_DIR/$EXT_DIR2_TARGET
+		fi
+	fi
 }
 
 start() {
-    if [ $MountedDirCount -gt 0 ]; then
-        echo "Chroot'ed services seems to be already started, exiting..."
-        exit 1
-    fi
+	if [ $MountedDirCount -gt 0 ]; then
+		echo "Chroot'ed services seems to be already started, exiting..."
+		exit 1
+	fi
 
-    echo "Starting chroot'ed Debian services..."
+	echo "Starting chroot'ed Debian services..."
 
-    for dir in dev proc sys; do
-        mount -o bind /$dir $CHROOT_DIR/$dir
-    done
-	
-    mount_ext
+	for dir in dev proc sys; do
+		mount -B /$dir $CHROOT_DIR/$dir
+	done
 
-    for item in $(cat $CHROOT_SERVICES_LIST); do
-        chroot $CHROOT_DIR /etc/init.d/$item start
-    done
+	mount_ext
+
+	for item in $(cat $CHROOT_SERVICES_LIST); do
+		chroot $CHROOT_DIR /etc/init.d/$item start
+	done
 }
 
 stop() {
-    if [ $MountedDirCount -eq 0 ]; then
-        echo "Chroot'ed services seems to be already stopped, exiting..."
-        exit 1
-    fi
+	if [ $MountedDirCount -eq 0 ]; then
+		echo "Chroot'ed services seems to be already stopped, exiting..."
+		exit 1
+	fi
 
-    echo "Stopping chroot'ed Debian services..."
+	echo "Stopping chroot'ed Debian services..."
 
-    for item in $(cat $CHROOT_SERVICES_LIST); do
-        chroot $CHROOT_DIR /etc/init.d/$item stop
-        sleep 2
-    done
+	for item in $(cat $CHROOT_SERVICES_LIST); do
+		chroot $CHROOT_DIR /etc/init.d/$item stop
+		sleep 2
+	done
 
-    umount /opt/debian/dev/pts
+	if mountpoint -q $CHROOT_DIR/dev/pts; then
+		umount $CHROOT_DIR/dev/pts
+	fi
 
-    mount | grep $CHROOT_DIR | awk '{print $3}' | xargs umount -l
+	mount | grep $CHROOT_DIR | awk '{print $3}' | xargs umount -l
 }
 
 restart() {
-    if [ $MountedDirCount -eq 0 ]; then
-        echo "Chroot'ed services seems to be already stopped"
-        start
-    else
-        echo "Stopping chroot'ed Debian services..."
-
-        for item in $(cat $CHROOT_SERVICES_LIST); do
-            chroot $CHROOT_DIR /etc/init.d/$item stop
-            sleep 2
-        done
-
-        mount | grep $CHROOT_DIR | awk '{print $3}' | xargs umount -l
-
-        echo "Restarting chroot'ed Debian services..."
-
-        for dir in dev proc sys; do
-            mount -o bind /$dir $CHROOT_DIR/$dir
-        done
-	
-        mount_ext
-
-        for item in $(cat $CHROOT_SERVICES_LIST); do
-            chroot $CHROOT_DIR /etc/init.d/$item start
-        done
-    fi
+	stop
+	start
 }
 
-enter() {	
-    mount_ext
+enter() {
+	mount_ext
 
-    mount -o bind /dev/ /opt/debian/dev/
-    mount -o bind /dev/pts /opt/debian/dev/pts
-    mount -o bind /proc/ /opt/debian/proc/
-    mount -o bind /sys/ /opt/debian/sys/
+	for dir in dev dev/pts proc sys; do
+		if ! mountpoint -q $CHROOT_DIR/$dir; then
+			mount -B /$dir $CHROOT_DIR/$dir
+		fi
+	done
 
-    chroot /opt/debian /bin/bash
+	chroot $CHROOT_DIR /bin/bash
 }
 
 status() {
-    if [ $MountedDirCount -gt 0 ]; then
-        echo "Chroot'ed services running..."
-    else
-        echo "Chroot'ed services not running!"
-    fi
+	if [ $MountedDirCount -gt 0 ]; then
+		echo "Chroot'ed services running..."
+	else
+		echo "Chroot'ed services not running!"
+	fi
 }
 
 case "$1" in
 start)
-    start
-    ;;
+	start
+	;;
 stop)
-    stop
-    ;;
+	stop
+	;;
 restart)
-    restart
-    ;;
+	restart
+	;;
 enter)
-    enter
-    ;;
+	enter
+	;;
 status)
-    status
-    ;;
+	status
+	;;
 *)
-    echo "Usage: (start|stop|restart|enter|status)"
-    exit 1
-    ;;
+	echo "Usage: (start|stop|restart|enter|status)"
+	exit 1
+	;;
 esac
+
 echo Done.
 exit 0
