71254fcfdd5060cdaa50b3dee23d0935e6b080e1
Chire <bbeny123@users.noreply.github.com>
2023-11-27 22:32:53 +0100

    Update mount-debian.sh

--- mount-debian.sh
+++ mount-debian.sh
@@ -10,11 +10,11 @@ DEBIAN_EXT_DIR2_TARGET=$DEBIAN_CHROOT_DIR/mnt/Beny-Kingston/
 if [ $(mount | grep $DEBIAN_CHROOT_DIR | wc -l) -gt 0 ]; then
   mkdir -p $DEBIAN_EXT_DIR1_TARGET
   if ! mountpoint -q $DEBIAN_EXT_DIR1_TARGET ; then
-    mount -r -o bind $DEBIAN_EXT_DIR1 $DEBIAN_EXT_DIR1_TARGET
+    mount -o bind $DEBIAN_EXT_DIR1 $DEBIAN_EXT_DIR1_TARGET
   fi
 
   mkdir -p $DEBIAN_EXT_DIR2_TARGET
   if ! mountpoint -q $DEBIAN_EXT_DIR2_TARGET ; then
-    mount -r -o bind $DEBIAN_EXT_DIR2 $DEBIAN_EXT_DIR2_TARGET
+    mount -o bind $DEBIAN_EXT_DIR2 $DEBIAN_EXT_DIR2_TARGET
   fi
 fi
